317729959669f6ab67585928ec83e292e43452da
sakhnaahan <saikhnaahan@gmail.com>
2025-11-16 16:40:46 +0800

    Shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ const page = () => {
   return (
     <>
       <h1 className="h1-bold">Tailwind Css is Fun</h1>
-      <h1 className="h1-bold font-space-grotesk">Tailwind Css is Fun</h1>
     </>
   );
 };
