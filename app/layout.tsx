c7a8a756b59afc50d38a30b14b7a142a3c46f3dc
sakhnaahan <saikhnaahan@gmail.com>
2025-11-18 21:25:07 +0800

    Implement github signIn

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,19 +2,19 @@ import type { Metadata } from "next";
 import "./globals.css";
 import localFont from "next/font/local";
 import { ThemeProvider } from "next-themes";
-
+import { Toaster } from "sonner";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
   variable: "--font-inter",
-  weight: "100 200 300 400 500 700 800 900"
+  weight: "100 200 300 400 500 700 800 900",
 });
 
 const spaceGrotesk = localFont({
   src: "./fonts/SpaceGroteskVF.ttf",
   variable: "--font-space-grotesk",
-  weight: "300 400 500 700"
-})
+  weight: "300 400 500 700",
+});
 
 export const metadata: Metadata = {
   title: "Dev Overflow",
@@ -32,18 +32,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
+      <body className={`${inter.className} ${spaceGrotesk.variable} antialiased`}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
       </body>
+      <Toaster />
     </html>
   );
 }
--- components/forms/SocialAuthForm.tsx
+++ components/forms/SocialAuthForm.tsx
@@ -1,14 +1,33 @@
+'use client'
 import React from "react";
 import { Button } from "../ui/button";
 import Image from "next/image";
+import { toast } from "sonner";
+
+
 
 const SocialAuthForm = () => {
   const buttonClass =
     "background-dark400_light900 body-medium text-dark200_light800 rounded-2 min-h-12 flex-1 px-4 py-3.5";
 
+  const handleSignIn = async (provider: "github" | "google") => {
+    try {
+      throw new Error('Not implemented')
+    } catch (error) {
+      console.log(error);
+
+      //! FIX TOAST
+     toast({
+       title: "Sign-in Failed",
+       description: error instanceof Error ? error.message : "An error occured during sign-in",
+       variant: "destructive",
+     });
+    }
+  };
+
   return (
     <div className="mt-10 flex flex-wrap gap-2.5">
-      <Button className={buttonClass}>
+      <Button className={buttonClass} onClick={() => handleSignIn("github")}>
         <Image
           src="/icons/github.svg"
           alt="Github Logo"
@@ -18,7 +37,7 @@ const SocialAuthForm = () => {
         />
         <span>Log in with Github</span>
       </Button>
-      <Button className={buttonClass}>
+      <Button className={buttonClass} onClick={() => handleSignIn("google")}>
         <Image src="/icons/google.svg" alt="Google Logo" width={20} height={20} className="ojbect-contain mr-2.5" />
         <span>Log in with Google</span>
       </Button>
